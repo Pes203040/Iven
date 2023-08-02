c0fe8e191a586036ec97714fc13a3542adeacb59
Pes203040 <99642050+Pes203040@users.noreply.github.com>
2023-08-02 02:56:05 +0100

    Update Choaib.py

--- Choaib.py
+++ Choaib.py
@@ -31,14 +31,17 @@ def clear():
 	os.system('clear')
 	print(logo)
 logo=("""\033[1;37m
-  .d8b.  db   dD d888888b d8b   db  d888b  
- d8' `8b 88 ,8P'   `88'   888o  88 88' Y8b 
- 88ooo88 88,8P      88    88V8o 88 88
- 88~~~88 88`8b      88    88 V8o88 88  ooo.
- 88   88 88 `88.   .88.   88  V888 88. ~8~
- YP   YP YP   YD Y888888P VP   V8P  Y888P   
+  ________ ______  
+        /        /      \ 
+        $$$$$$$$/$$$$$$  |
+           $$ | $$____$$ |
+           $$ |  /    $$/ 
+           $$ | /$$$$$$/  
+           $$ | $$ |_____     MAFIA-T2
+           $$ | $$       |
+           $$/  $$$$$$$$/         
 ----------------------------------------------
- Author    : IMTIAZ AKING
+ Author    : yacine 19
  Status    : PAID
  Version   : 8.7.2 \033[1;37m
  ok ids will be saved in AKING folder
