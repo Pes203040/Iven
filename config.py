813358068686d38e052b19724cb90851d0a00313
Pes203040 <99642050+Pes203040@users.noreply.github.com>
2023-08-21 02:42:03 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Telegram API configuration
+API_ID = "28909113"
+API_HASH = "11236c9471502d5bde6519534a4dc0f4"
+PHONE_NUMBER = "+213655139288"
+
+# Bot configuration
+CHANNEL = "https://t.me/dark_team_xv"  # The channel URL or username
+LOG_CHANNEL = "@your_log_channel"  # The log channel username or ID
+BOT_USERNAME = "@Bekoma_bot"  # Your bot's username
+
+# MongoDB configuration
+MONGO_URI = "mongodb://localhost:27017"  # Your MongoDB connection URI
+DATABASE_NAME = "telegram_bot"  # The name of the MongoDB database
+COLLECTION_NAME = "bot_data"  # The name of the MongoDB collection
+
+# Picture configuration
+PIC_URL = "https://www.pinterest.com/hnn0022/photo-de-profil-hijab/&ved=2ahUKEwjZ4KyGz-yAAxXE0wIHHYxnAKAQr4kDegQIDxAD&usg=AOvVaw1vGT2X_gk8CVmftaaijCdp"  # The URL of the picture to include
